b35e80b573e56d80d36bf02c7de4821229921f8f
Patrick Mueller <pmuellr@apache.org>
2015-04-12 11:32:19 -0400

    browser code working

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,15 +1,17 @@
 $(onLoad)
 
+var lastSeen = -1
+
 //------------------------------------------------------------------------------
 function onLoad() {
-  uiLoad("loaded")
-  setInterval( 500, refreshData)
-  setInterval(1000, generateNewData)
+  uiLog("loaded")
+  setInterval(refreshData,      300)
+  setInterval(generateNewData, 1000)
 }
 
 //------------------------------------------------------------------------------
-function uilog(string) {
-  $(".console").append(string + "\n")
+function uiLog(string) {
+  $("#console").append(string + "\n")
 }
 
 //------------------------------------------------------------------------------
@@ -23,7 +25,25 @@ function refreshData() {
 
 //------------------------------------------------------------------------------
 function refreshSuccess(data, status, jqXHR) {
-  uiLog(JSON.stringify(data))
+  //uiLog("refreshData: " + JSON.stringify(data))
+  if (!data) return
+
+  var items = data.data
+  if (!items) return
+
+  var foundSome = false
+  for (var i=0; i< items.length; i++) {
+    var item = items[i]
+    if (item.index <= lastSeen) continue
+
+    lastSeen  = item.index
+    foundSome = true
+    uiLog("got " + item.datum)
+  }
+
+  if (!foundSome) {
+    uiLog("no new items")
+  }
 }
 
 //------------------------------------------------------------------------------
@@ -33,4 +53,23 @@ function refreshError(jqXHR, status, error) {
 
 //------------------------------------------------------------------------------
 function generateNewData() {
+  $.ajax("/api/data", {
+    method:   "POST",
+    data: {
+      datum: JSON.stringify("" + new Date())
+    },
+    dataType: "json",
+    success:  generateSuccess,
+    error:    generateError
+  })
+}
+
+//------------------------------------------------------------------------------
+function generateSuccess(data, status, jqXHR) {
+  // uiLog("generateNewData: " + JSON.stringify(data))
+}
+
+//------------------------------------------------------------------------------
+function generateError(jqXHR, status, error) {
+  console.log("error refreshing: ", error)
 }
